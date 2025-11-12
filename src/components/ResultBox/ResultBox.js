d42d8ba2c793fcd9e0f2022e2e89578e939139db
Rafal Labunski <rafallabunski@gmail.com>
2025-11-12 10:44:38 +0100

    ResultBox: handle negative amount (render 'Wrong value…' for amount < 0)

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -6,14 +6,24 @@ import { useMemo } from 'react';
 import styles from './ResultBox.module.scss';
 
 const ResultBox = ({ from, to, amount }) => {
-
   const convertedAmount = useMemo(() => {
-    if(from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
-    if(from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
+    if (from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
+    if (from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
     return formatAmountInCurrency(amount, from);
   }, [from, to, amount]);
 
-  const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
+  const formattedAmount = useMemo(
+    () => formatAmountInCurrency(amount, from),
+    [amount, from]
+  );
+
+  if (amount < 0) {
+    return (
+      <div data-testid="output" className={styles.result}>
+        Wrong value…
+      </div>
+    );
+  }
 
   return (
     <div data-testid="output" className={styles.result}>
@@ -26,6 +36,6 @@ ResultBox.propTypes = {
   from: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
-}
+};
 
 export default ResultBox;
